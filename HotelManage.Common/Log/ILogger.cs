0abdd6cc4170a0efb1c4de3cdd4f6d33e0e404c3
wangshuaiw <834283444@qq.com>
2018-06-24 16:50:14 +0800

    Log common

---
+++ HotelManage.Common/Log/ILogger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+namespace HotelManage.Common.Log
+{
+    /// <summary>
+    ///  Log interface
+    /// </summary>
+    public interface ILogger
+    {
+        void Debug(object msg);
+
+        void Info(object msg);
+
+        void Error(object msg);
+
+        void Error(object msg, Exception ex);
+    }
+}
---
+++ HotelManage.Common/Log/Log4Helper.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using log4net;
+
+namespace HotelManage.Common.Log
+{
+    public class Log4Helper : ILogger
+    {
+        private ILog logger;
+
+        //定义一个静态变量来保存类的实例
+        private static Log4Helper _instance = null;
+
+        //定义一个标识确保线程同步
+        private static object obj;
+
+        //定义私有构造函数，使外界不能创建该类实例
+        private Log4Helper()
+        {
+            logger = LogManager.GetLogger(typeof(Log4Helper));
+        }
+
+        //可以定义公有属性来提供全局访问点
+        public static Log4Helper Instance
+        {
+            get
+            {
+                if(_instance==null)
+                {
+                    lock(obj)
+                    {
+                        _instance = new Log4Helper();
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        public void Debug(object msg)
+        {
+            logger.Debug(msg);
+        }
+
+        public void Error(object msg)
+        {
+            logger.Error(msg);
+        }
+
+        public void Error(object msg, Exception ex)
+        {
+            logger.Error(msg, ex);
+        }
+
+        public void Info(object msg)
+        {
+            logger.Info(msg);
+        }
+    }
+}
